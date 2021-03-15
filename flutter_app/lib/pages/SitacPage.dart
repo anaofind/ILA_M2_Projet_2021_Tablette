0dd1b6e1120a06ef86b6bee179750db344d8b128
Marquer Kevin <kevin.marquer@etudiant.univ-rennes1.fr>
2021-03-15 14:47:57 +0100

    commit Sitac + icone

--- flutter_app/lib/pages/SitacPage.dart
+++ flutter_app/lib/pages/SitacPage.dart
@@ -34,17 +34,19 @@ class SitacPageState extends State<SitacPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DefaultTabController(
-        length: 6,
+        length: 7,
         child: Scaffold(
           appBar: AppBar(
             bottom: TabBar(
               tabs: [
-                Tab(icon: Image(image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),)),
+                Tab(icon: Image(image: new AssetImage('Icone_Png/Infrastructure_Basic.png'),)),
                 Tab(icon: Image(image: new AssetImage('Icone_Png/Danger/CibleAction_Basic.png'),)),
                 Tab(icon: Image(image: new AssetImage('Icone_Png/Danger/Danger_Basic.png'),)),
-                Tab(icon: Image(image: new AssetImage('Icone_Png/PointEau_Perienne.png'),)),
-                Tab(icon: Image(image: new AssetImage('Icone_Png/Défense/Perimetrale_Basic_Encours.png'),)),
+                Tab(icon: Image(image: new AssetImage('Icone_Png/PointEau.png'),)),
+                Tab(icon: Image(image: new AssetImage('Icone_Png/Defense/Perimetrale_Basic_Prevue.png'),)),
+                Tab(icon: Image(image: new AssetImage('Icone_Png/Chemin/Chemin_Encours.png'),)),
                 Tab(icon: Image(image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),)),
+
               ],
             ),
             title: Text('Tabs Demo'),
@@ -58,20 +60,52 @@ class SitacPageState extends State<SitacPage> {
                     padding: const EdgeInsets.all(8),
                     children: <Widget>[
                       Image(
-                        image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png' ),
-                        width: 50,
-                        height: 50,
+                        image: new AssetImage('Icone_Png/Infrastructure/Vehicule/FPT/FPT_Unique_Encours.png' ),
+                        width: 100,
+                        height: 100,
                       ),
                       Image(
-                        image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),
-                        width: 50,
-                        height: 50,
+                        image: new AssetImage('Icone_Png/Infrastructure/Vehicule/FPT/FPT_Unique_Prevue.png'),
+                        width: 100,
+                        height: 100,
                       ),
 
                       Image(
-                        image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),
-                        width: 50,
-                        height: 50,
+                        image: new AssetImage('Icone_Png/Infrastructure/Vehicule/VLCG/VLGC_Unique_Rouge_Encours.png'),
+                        width: 100,
+                        height: 100,
+                      ),
+                      Image(
+                        image: new AssetImage('Icone_Png/Infrastructure/Vehicule/VLCG/VLGC_Unique_Rouge_Prevue.png'),
+                        width: 100,
+                        height: 100,
+                      ),
+
+                      Image(
+                        image: new AssetImage('Icone_Png/Infrastructure/Vehicule/VLCG/VLGC_Unique_Vert_Encours.png'),
+                        width: 100,
+                        height: 100,
+                      ),
+                      Image(
+                        image: new AssetImage('Icone_Png/Infrastructure/Vehicule/VLCG/VLGC_Unique_Vert_Prevue.png'),
+                        width: 100,
+                        height: 100,
+                      ),
+
+                      Image(
+                        image: new AssetImage('Icone_Png/Infrastructure/Vehicule/VSAV/VSAV_Unique_Encours.png'),
+                        width: 100,
+                        height: 100,
+                      ),
+                      Image(
+                        image: new AssetImage('Icone_Png/Infrastructure/Vehicule/VSAV/VSAV_Unique_Prevue.png'),
+                        width: 100,
+                        height: 100,
+                      ),
+                      Image(
+                        image: new AssetImage('Icone_Png/PosteCommandement_Seul_Encours.png'),
+                        width: 75,
+                        height: 75,
                       ),
                     ],
                   ),
@@ -80,20 +114,25 @@ class SitacPageState extends State<SitacPage> {
                     padding: const EdgeInsets.all(8),
                     children: <Widget>[
                       Image(
-                        image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png' ),
-                        width: 50,
-                        height: 50,
+                        image: new AssetImage('Icone_Png/Danger/CibleAction_Incendie.png' ),
+                        width: 100,
+                        height: 100,
                       ),
                       Image(
-                        image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),
-                        width: 50,
-                        height: 50,
+                        image: new AssetImage('Icone_Png/Danger/CibleAction_Civil.png' ),
+                        width: 100,
+                        height: 100,
+                      ),
+                      Image(
+                        image: new AssetImage('Icone_Png/Danger/CibleAction_Eau.png'),
+                        width: 100,
+                        height: 100,
                       ),
 
                       Image(
-                        image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),
-                        width: 50,
-                        height: 50,
+                        image: new AssetImage('Icone_Png/Danger/CibleAction_Particulier.png'),
+                        width: 100,
+                        height: 100,
                       ),
                     ],
                   ),
@@ -102,20 +141,48 @@ class SitacPageState extends State<SitacPage> {
                     padding: const EdgeInsets.all(8),
                     children: <Widget>[
                       Image(
-                        image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png' ),
-                        width: 50,
-                        height: 50,
+                        image: new AssetImage('Icone_Png/Danger/Danger_Incendie.png' ),
+                        width: 100,
+                        height: 100,
                       ),
                       Image(
-                        image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),
-                        width: 50,
-                        height: 50,
+                        image: new AssetImage('Icone_Png/Danger/Danger_Civil.png'),
+                        width: 100,
+                        height: 100,
                       ),
 
                       Image(
-                        image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),
-                        width: 50,
-                        height: 50,
+                        image: new AssetImage('Icone_Png/Danger/Danger_Eau.png'),
+                        width: 100,
+                        height: 100,
+                      ),
+                      Image(
+                        image: new AssetImage('Icone_Png/Danger/Danger_Particulier.png'),
+                        width: 100,
+                        height: 100,
+                      ),
+
+                      //Point Sensible
+                      Image(
+                        image: new AssetImage('Icone_Png/Danger/PointSensible_Incendie.png' ),
+                        width: 100,
+                        height: 100,
+                      ),
+                      Image(
+                        image: new AssetImage('Icone_Png/Danger/PointSensible_Civil.png'),
+                        width: 100,
+                        height: 100,
+                      ),
+
+                      Image(
+                        image: new AssetImage('Icone_Png/Danger/PointSensible_Eau.png'),
+                        width: 100,
+                        height: 100,
+                      ),
+                      Image(
+                        image: new AssetImage('Icone_Png/Danger/PointSensible_Particulier.png'),
+                        width: 100,
+                        height: 100,
                       ),
                     ],
                   ),
@@ -124,20 +191,20 @@ class SitacPageState extends State<SitacPage> {
                     padding: const EdgeInsets.all(8),
                     children: <Widget>[
                       Image(
-                        image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png' ),
-                        width: 50,
-                        height: 50,
+                        image: new AssetImage('Icone_Png/PointEau_Perienne.png' ),
+                        width: 75,
+                        height: 75,
                       ),
                       Image(
-                        image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),
-                        width: 50,
-                        height: 50,
+                        image: new AssetImage('Icone_Png/PointEau_NonPerienne.png'),
+                        width: 75,
+                        height: 75,
                       ),
 
                       Image(
-                        image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),
-                        width: 50,
-                        height: 50,
+                        image: new AssetImage('Icone_Png/PointEau_Ravitaillement.png'),
+                        width: 75,
+                        height: 75,
                       ),
                     ],
                   ),
@@ -146,52 +213,104 @@ class SitacPageState extends State<SitacPage> {
                 padding: const EdgeInsets.all(8),
                 children: <Widget>[
                   Image(
-                    image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png' ),
-                    width: 50,
-                    height: 50,
-                  ),
-                  Image(
-                    image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),
-                    width: 50,
-                    height: 50,
+                    image: new AssetImage('Icone_Png/Defense/Defense_Incendie_Encours.png' ),
+                    width: 100,
+                    height: 100,
                   ),
 
                   Image(
-                    image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),
-                    width: 50,
-                    height: 50,
+                    image: new AssetImage('Icone_Png/Defense/Defense_Civil_Encours.png'),
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image(
+                    image: new AssetImage('Icone_Png/Defense/Defense_Eau_Encours.png'),
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image(
+                    image: new AssetImage('Icone_Png/Defense/Defense_Particulier_Encours.png'),
+                    width: 100,
+                    height: 100,
+                  ),
+                  //Perimetrale
+                  Image(
+                    image: new AssetImage('Icone_Png/Defense/Perimetrale_Incendie_Encours.png' ),
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image(
+                    image: new AssetImage('Icone_Png/Defense/Perimetrale_Civil_Encours.png'),
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image(
+                    image: new AssetImage('Icone_Png/Defense/Perimetrale_Eau_Encours.png'),
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image(
+                    image: new AssetImage('Icone_Png/Defense/Perimetrale_Particulier_Encours.png'),
+                    width: 100,
+                    height: 100,
                   ),
                 ],
               ),
+
+                  ListView(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.all(8),
+                    children: <Widget>[
+                      Image(
+                        image: new AssetImage('Icone_Png/Chemin/Chemin_Encours.png' ),
+                        width: 100,
+                        height: 100,
+                      ),
+                      SizedBox(height: 10),
+                      Image(
+                        image: new AssetImage('Icone_Png/Chemin/Chemin_Prevue.png'),
+                        width: 100,
+                        height: 100,
+                      ),
+
+                    ],
+                  ),
+
               ListView(
                 scrollDirection: Axis.horizontal,
                 padding: const EdgeInsets.all(8),
                 children: <Widget>[
                   Image(
                     image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png' ),
-                    width: 50,
-                    height: 50,
+                    width: 100,
+                    height: 100,
                   ),
                   Image(
                     image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),
-                    width: 50,
-                    height: 50,
+                    width: 100,
+                    height: 100,
                   ),
 
                   Image(
                     image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),
-                    width: 50,
-                    height: 50,
+                    width: 100,
+                    height: 100,
                   ),
                 ],
               ),
+
+
             ],
           ),
-              Image(
-                image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),
-                width: 50,
-                height: 50,
+              Positioned(
+                bottom: 0,
+                child: Image(
+                  image: new AssetImage('Icone_Png/Infrastructure_Civil_EnCours.png'),
+                  width: 50,
+                  height: 50,
+                ),
               ),
+
             ],
           ),
         ),
